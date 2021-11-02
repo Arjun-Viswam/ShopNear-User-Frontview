68e6763b19560c651497057648028dc8fc61d70a
Arjun Viswam <arjunviswam82@gmail.com>
2021-11-02 14:02:32 +0530

    second commit

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -1,38 +1,40 @@
 import React from "react";
-import "./Body.css";
+import style from "./Body.module.css";
 import { Card } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function Body() {
   return (
-    <div>
-      <div className="containerz" >
-        <div>
-        <div style={{position:'relative',backgroundColor:"#cccece"}} className="row">
-          <div className='col-12 col-md-6' style={{display:"flex",justifyContent:"center",alignSelf:"center"}}>
-          <h1 style={{fontSize:"bolder"}}>Shop from your <br/> Near Store</h1>
+    <div className={`${style.Body} container`}>
+      <div className={style.row}>
+      <div className={`${style.text_area} col-12 col-md-3`}>
+            
+            <h1 className={style.text_bold}>
+              Shop from your Near Store
+            </h1>
           </div>
-          <div className='col-12 col-md-6 p-5'>
-        <img className="BodyImage" src="/images/favpng.png" alt="" />  
-          </div>
-          
-          <div style={{position:'absolute',display:"flex",justifyContent:"center",alignSelf:"center"}}>
-            <div style={{maxWidth:"400px",margin:"auto"}}>
-            <div className="input-icons">
-          <i class="fas fa-map-marker-alt"></i>
-            <input class="input-field" type="text" placeholder="Current Location" name="" id="" />
+          <div className={style.location}>
+                <i class="fas fa-map-marker-alt"></i>
+                <input
+                  class={style.input_icons}
+                  type="text"
+                  placeholder="Current Location"
+                  name=""
+                  id=""
+                />
             </div>
+          <div className="col-12 col-md-9">
+            <img className={`${style.image}`} src="/images/favpng.png" alt="" />
+          </div>
+      </div>
+      <h1>&nbsp;</h1>
+      <div className="row">
+          <div className={style.feature}>
+            <div className={style.titleOne}>
+              <h1 className={style.titleTwo}>AMAZING FEATURES</h1>
             </div>
           </div>
-        </div>
-        </div>
-        <div className="row">
-          <div className="feature">
-            <div className="titleOne">
-              <h1 className='titleTwo'>AMAZING FEATURES</h1>
-            </div>
-          </div>
-          <div className="cardIn  col-12 col-md-6" style={{display:"flex",justifyContent:"right",marginTop:"80px"}}>
+          <div className= {`${style.cardIn} col-12 col-md-6`} style={{display:"flex",justifyContent:"right",marginTop:"80px"}}>
           <Card style={{ width: "40rem",border:"none" }}>
               <Card.Body style={{backgroundColor:"#e2e5e6"}}>
               <img src='/images/PikPng.png' style={{width:'121px',height:'96px',float:"left"}} alt='' />
@@ -43,18 +45,18 @@ function Body() {
               </Card.Body>
             </Card> 
           </div>
-          <div className="cardIn  col-12 col-md-6" style={{display:"flex",justifyContent:"left",marginTop:"80px"}}>
+          <div className={`${style.cardIn} col-12 col-md-6`} style={{display:"flex",justifyContent:"left",marginTop:"80px"}}>
           <Card style={{ width: "40rem",border:"none" }}>
               <Card.Body style={{backgroundColor:"#e2e5e6"}}>
               <img src='/images/PikPng.png' style={{width:'121px',height:'96px',float:"left"}} alt='' />
-                <Card.Title><h2>Accept Suggested Substitutes</h2></Card.Title>
+                <Card.Title><h2>Accept Substitutes</h2></Card.Title>
                 <Card.Text>
                 <h6>In case your specific item isn’t available,<br/> you can choose from the substitutes suggested <br/> by your merchant.</h6>
                 </Card.Text>
               </Card.Body>
             </Card> 
           </div>
-          <div className="cardIn  col-12 col-md-6" style={{display:"flex",justifyContent:"right",marginTop:"80px"}}>
+          <div className={`${style.cardIn} col-12 col-md-6`} style={{display:"flex",justifyContent:"right",marginTop:"80px"}}>
           <Card style={{ width: "40rem",border:"none" }}>
               <Card.Body style={{backgroundColor:"#e2e5e6"}}>
               <img src='/images/PikPng.png' style={{width:'121px',height:'96px',float:"left"}} alt='' />
@@ -65,7 +67,7 @@ function Body() {
               </Card.Body>
             </Card> 
           </div>
-          <div className="cardIn  col-12 col-md-6" style={{display:"flex",justifyContent:"left",marginTop:"80px"}}>
+          <div className={`${style.cardIn} col-12 col-md-6`} style={{display:"flex",justifyContent:"left",marginTop:"80px"}}>
           <Card style={{ width: "40rem",border:"none" }}>
               <Card.Body style={{backgroundColor:"#e2e5e6"}}>
               <img src='/images/PikPng.png' style={{width:'121px',height:'96px',float:"left"}} alt='' />
@@ -76,7 +78,7 @@ function Body() {
               </Card.Body>
             </Card> 
           </div>
-          <div className="cardIn  col-12 col-md-6" style={{display:"flex",justifyContent:"right",marginTop:"80px"}}>
+          <div className={`${style.cardIn} col-12 col-md-6`} style={{display:"flex",justifyContent:"right",marginTop:"80px"}}>
           <Card style={{ width: "40rem",border:"none" }}>
               <Card.Body style={{backgroundColor:"#e2e5e6"}}>
               <img src='/images/PikPng.png' style={{width:'121px',height:'96px',float:"left"}} alt='' />
@@ -87,7 +89,7 @@ function Body() {
               </Card.Body>
             </Card> 
           </div>
-          <div className="cardIn col-12 col-md-6" style={{display:"flex",justifyContent:"left",marginTop:"80px"}}>
+          <div className={`${style.cardIn} col-12 col-md-6`} style={{display:"flex",justifyContent:"left",marginTop:"80px"}}>
           <Card style={{ width: "40rem",border:"none" }}>
               <Card.Body style={{backgroundColor:"#e2e5e6"}}>
               <img src='/images/PikPng.png' style={{width:'121px',height:'96px',float:"left"}} alt='' />
@@ -100,60 +102,126 @@ function Body() {
           </div>
           <h1>&nbsp;</h1>
         </div>
-        
-      </div>
+
       <div className="row">
-          <div className="feature">
-            <div className="titleOne">
-              <h1 className='titleTwo'>AWESOME BENEFITS</h1>
-            </div>
+        <div className={style.feature}>
+          <div className={style.titleOne}>
+            <h1 className={style.titleTwo}>AWESOME BENEFITS</h1>
           </div>
-          <div className="cardIn col-12 col-md-4" style={{display:"flex",justifyContent:"center",marginTop:"80px"}}>
-          <Card style={{ width: '28rem',backgroundColor:"#e2e5e6",border:"none" }}>
-              <Card.Img variant="top" style={{width:"51%",alignSelf:"center"}} src='/images/PikPng.png' />
-              <Card.Body >
-                <Card.Title><h3>Order Anything You Want</h3></Card.Title>
-                <Card.Text>
-                Conveniently order essentials, groceries and a whole variety of products – branded or unbranded – from your nearest stores. Find which store is known for what and order from the best.
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </div>
-          <div className="cardIn col-12 col-md-4" style={{display:"flex",justifyContent:"center",marginTop:"80px"}}>
-          <Card style={{ width: '28rem',backgroundColor:"#e2e5e6",border:"none" }}>
-          <Card.Img variant="top" style={{width:"51%",alignSelf:"center"}} src='/images/PikPng.png' />
-          <Card.Body >
-            <Card.Title><h3>Order Smartly. Avoid Confusions</h3></Card.Title>
-            <Card.Text>
-            Avoid the order confusions and mismatches that generally happen over a phone call. Now, confirm all order specifications and modifications on the app, and get only the perfect orders delivered.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-          </div>
-          
-          <div className="cardIn col-12 col-md-4" style={{display:"flex",justifyContent:"center",marginTop:"80px"}}>
-          <Card style={{ width: '28rem',backgroundColor:"#e2e5e6",border:"none" }}>
-            <Card.Img variant="top" style={{width:"51%",alignSelf:"center"}} src='/images/PikPng.png' />
-            <Card.Body >
-              <Card.Title><h3>Get Better Discounts & Offers</h3></Card.Title>
+        </div>
+        <div
+          className={`${style.cardIn} col-12 col-md-4`}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "80px",
+          }}
+        >
+          <Card
+            style={{
+              width: "28rem",
+              backgroundColor: "#e2e5e6",
+              border: "none",
+            }}
+          >
+            <Card.Img
+              variant="top"
+              style={{ width: "51%", alignSelf: "center" }}
+              src="/images/PikPng.png"
+            />
+            <Card.Body>
+              <Card.Title>
+                <h3>Order Anything You Want</h3>
+              </Card.Title>
               <Card.Text>
-              Save more by easily checking which nearby store is offering relevant discounts and offers before you purchase your staples.
+                Conveniently order essentials, groceries and a whole variety of
+                products – branded or unbranded – from your nearest stores. Find
+                which store is known for what and order from the best.
               </Card.Text>
             </Card.Body>
           </Card>
-          </div>
-          <h1>&nbsp;</h1>
-         
         </div>
-          <div className="feature">
-            <div className="titleOne">
-              <h1 className='titleTwo col-12'>HOW IT WORKS</h1>
-            </div>
-          </div>
-          <h6 className='text'>Find your current location. enter the products you need to buy in the column given. The website automatically finds out the shops nearby your location which have the particular items in the shop. then it will show you some of the plan by which you can easily shop the items you required. this is how the shop near works for you.</h6>
+        <div
+          className={`${style.cardIn} col-12 col-md-4`}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "80px",
+          }}
+        >
+          <Card
+            style={{
+              width: "28rem",
+              backgroundColor: "#e2e5e6",
+              border: "none",
+            }}
+          >
+            <Card.Img
+              variant="top"
+              style={{ width: "51%", alignSelf: "center" }}
+              src="/images/PikPng.png"
+            />
+            <Card.Body>
+              <Card.Title>
+                <h3>Order Smartly. Avoid Confusions</h3>
+              </Card.Title>
+              <Card.Text>
+                Avoid the order confusions and mismatches that generally happen
+                over a phone call. Now, confirm all order specifications and
+                modifications on the app, and get only the perfect orders
+                delivered.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+
+        <div
+          className={`${style.cardIn} col-12 col-md-4`}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "80px",
+          }}
+        >
+          <Card
+            style={{
+              width: "28rem",
+              backgroundColor: "#e2e5e6",
+              border: "none",
+            }}
+          >
+            <Card.Img
+              variant="top"
+              style={{ width: "51%", alignSelf: "center" }}
+              src="/images/PikPng.png"
+            />
+            <Card.Body>
+              <Card.Title>
+                <h3>Get Better Discounts & Offers</h3>
+              </Card.Title>
+              <Card.Text>
+                Save more by easily checking which nearby store is offering
+                relevant discounts and offers before you purchase your staples.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+        <h1>&nbsp;</h1>
       </div>
+      <div className={style.feature}>
+        <div className={style.titleOne}>
+          <h1 className={`${style.titleTwo} col-12`}>HOW IT WORKS</h1>
+        </div>
+      </div>
+      <h6 className={style.text}>
+        Find your current location. enter the products you need to buy in the
+        column given. The website automatically finds out the shops nearby your
+        location which have the particular items in the shop. then it will show
+        you some of the plan by which you can easily shop the items you
+        required. this is how the shop near works for you.
+      </h6>
+    </div>
   );
 }
 
 export default Body;
-
